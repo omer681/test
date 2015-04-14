cbcb0d3f8445194bfc26124c808f10e8cfc2d8cf
Omer <omerg681@gmail.com>
2015-04-14 19:49:35 +0300

    styling homepage

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			watchHtml: {
-				files: ['*.html', '*.js', '*.css'],
+				files: ['*.html', 'js/*.js', 'css/*.css'],
 				tasks: [],
 				options: {
 					livereload: true
